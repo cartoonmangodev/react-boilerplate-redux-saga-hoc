7133261457c5e70e8a39d8c01e36c41a3fecf9f7
chrissieaerospace <chrissieaerospace@gmail.com>
2020-08-14 17:33:49 +0530

    [MODIFIED] - delete handler

--- es/utils/customHandlers/deleteKeyHandler.js
+++ es/utils/customHandlers/deleteKeyHandler.js
@@ -7,16 +7,30 @@ exports.deleteKeyHandler = void 0;
 
 var _helpers = require("../helpers");
 
+var _remove2 = _interopRequireDefault(require("lodash/remove"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
 
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-var deletedData = function deletedData(obj, keyArray) {
-  return Object.keys(obj).reduce(function (acc, curr) {
-    return keyArray.includes(curr) && acc || _objectSpread({}, acc, _defineProperty({}, curr, obj[curr]));
-  }, {});
+var deletedData = function deletedData() {
+  var obj = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+  var keyArray = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [];
+  var _obj = obj;
+  _obj = Object.prototype.toString.call(obj) === '[object Object]' ? _objectSpread({}, _obj) : {};
+
+  if (Object.prototype.toString.call(obj) === '[object Object]') {
+    (Array.isArray(keyArray) ? keyArray : [keyArray]).forEach(function (_key) {
+      _obj = Array.isArray(_key) ? (0, _helpers.deleteIn)(_obj, _key) : (0, _helpers.deleteIn)(_obj, [_key]);
+    });
+    return _obj;
+  }
+
+  return obj;
 };
 
 var deleteKeyHandler = function deleteKeyHandler(_ref) {
