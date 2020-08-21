4b200343809f26ce9c1593bd7c3be9422c43849f
chrissieaerospace <chrissieaerospace@gmail.com>
2020-08-21 20:45:14 +0530

    [MODIFIED]

--- es/containers/Authentication/tests/selectors.test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-// import { selectDashboardDomain } from '../selectors';
-describe('selectDashboardDomain', function () {
-  it('Expect to have unit tests specified', function () {
-    expect(true).toEqual(false);
-  });
-});
