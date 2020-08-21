adfd1ea06b85533d7e39e0b61f7500af45cc69b0
chrissieaerospace <chrissieaerospace@gmail.com>
2020-08-21 20:59:29 +0530

    [MODIFIED] - package.json

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
 /* eslint-disable global-require */
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./dist/react-boilerplate-redux-saga-hoc.production.min.js');
-} else {
-  module.exports = require('./dist/react-boilerplate-redux-saga-hoc.development.js');
-}
+module.exports = require('./dist/react-boilerplate-redux-saga-hoc.production.min.js');
