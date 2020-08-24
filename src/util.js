66d6ee05eeb972d6dfe94060d1e931c9876b5846
chrissieaerospace <chrissieaerospace@gmail.com>
2020-08-24 19:51:05 +0530

    [MODIFIED] - utils

--- src/util.js
+++ src/util.js
@@ -3,7 +3,6 @@ export { default as IndianStates } from './utils/indianStates';
 // export * as Regex from './utils/regex';
 export { default as FormValidator } from './utils/formValidation';
 export { getData, toPromise, useStaleRefresh } from './utils';
-export { useDispatch, useStore, useSelector } from 'react-redux';
 // export { useTestHook } from './hooks';
 export {
   cloneObject,
@@ -17,14 +16,6 @@ export {
   toCapitalize,
   typeOf,
 } from './utils/helpers';
-export { default as store, nextStore } from './utils/configureStore';
 export { commonConstants } from './utils/commonReduxSagaConverter/commonConstants';
-export {
-  default as injectReducer,
-  useInjectReducer,
-} from './utils/utils/injectReducer';
-export { default as injectSaga, useInjectSaga } from './utils/utils/injectSaga';
-export { default as axios } from './config/axios';
 export { default as withReduxSaga } from './utils/utils/next/withReduxSaga';
 export { default as CustomError } from './utils/customError';
-export { default as withRedux } from './utils/utils/next/withRedux';
