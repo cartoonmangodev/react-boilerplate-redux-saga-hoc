e5cab380458a5fbff33d526d8167b98bcfa7c6fa
chrissieaerospace <chrissieaerospace@gmail.com>
2020-08-25 12:07:46 +0530

    [MODIFIED] - git

--- src/utils/reducers.js
+++ src/utils/reducers.js
@@ -6,35 +6,17 @@
  */
 
 import { combineReducers } from 'redux';
-// import { connectRouter } from 'connected-react-router';
-
-// import history from './utils/history';
-// import globalReducer from 'containers/App/reducer';
-// import languageProviderReducer from 'containers/LanguageProvider/reducer';
 
 /**
  * Merges the main reducer with the router state and dynamically injected reducers
  */
 export default function createReducer(injectedReducers = {}) {
-  // const connectRouter = history
-  //   ? require('connected-react-router').connectRouter
-  //   : null;
-  // const History = history ? require('./utils/history').default : '';
   const reducer =
     Object.keys(injectedReducers).length > 0
       ? injectedReducers
       : {
           global: () => ({}),
         };
-  // const reducer = history
-  //   ? {
-  //       ..._reducer,
-  //       router: connectRouter(History),
-  //     }
-  //   : {
-  //       ..._reducer,
-  //     };
   const rootReducer = combineReducers(reducer);
-
   return rootReducer;
 }
