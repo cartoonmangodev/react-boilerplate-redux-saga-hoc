ff5a3c052a16bfca779af9b25204e95b38825263
chrissieaerospace <chrissieaerospace@gmail.com>
2022-09-18 22:14:54 +0530

    [CLEANED-UP] - CODE

--- src/util.js
+++ src/util.js
@@ -1,3 +1,4 @@
+import { commonConstants } from './utils/commonReduxSagaConverter/commonConstants';
 export { default as Safe } from './utils/nullCheck';
 export { default as IndianStates } from './utils/indianStates';
 export { default as FormValidator } from './utils/formValidation';
@@ -30,3 +31,43 @@ export { default as withReduxSaga } from './utils/utils/next/withReduxSaga';
 export { default as CustomError } from './utils/customError';
 export { default as withRedux } from './utils/utils/next/withRedux';
 export { default as injectSaga, useInjectSaga } from './utils/utils/injectSaga';
+export const {
+  INFINITE_DATA_HANDLER,
+  DATA_HANDLER,
+  DELETE_DATA_HANDLER,
+  UPDATE_DATA_HANDLER,
+  UPDATE_DATA_KEY_HANDLER,
+  DELETE_DATA_KEY_HANDLER,
+  TOGGLE_DATA_KEY_HANDLER,
+  SPLICE_DATA_HANDLER,
+  RESET_HANDLER,
+  CALLBACK_HANDLER,
+  TOAST_HANDLER,
+  ERROR_HANDLER,
+  LOADER_HANDLER,
+  DONT_UPDATE_DATA_HANDLER,
+  ON_CANCEL_ERROR,
+  ON_ERROR,
+  ON_SUCCESS,
+  ON_FINALLY,
+  ON_CANCEL,
+  ON_REQUEST,
+  ON_LOADING,
+  ON_UNMOUNT,
+  ON_TOAST,
+  ERROR,
+  SUCCESS,
+  CALL,
+  CANCEL,
+  CUSTOM,
+  TYPE_NULL,
+  TYPE_UNDEFINED,
+  TYPE_STRING,
+  TYPE_ARRAY,
+  TYPE_BOOLEAN,
+  TYPE_OBJECT,
+  TYPE_FUNCTION,
+  TYPE_ERROR,
+  TYPE_SYMBOL,
+  TYPE_GENERATOR_FUNCTION,
+} = commonConstants;
