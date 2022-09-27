60a10680055a12d24d17fc384577e96f8c3be559
chrissieaerospace <chrissieaerospace@gmail.com>
2022-09-28 03:41:42 +0530

    [CLEANED-UP] - CODE

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ export { HOC } from './containers';
 export { default as Safe } from './utils/nullCheck';
 export { default as store, nextStore } from './utils/configureStore';
 export { commonConstants } from './utils/commonReduxSagaConverter/commonConstants';
-// export { default as axios } from './config/axios';
+export { default as axios } from './config/axios';
 export { default as withReduxSaga } from './utils/utils/next/withReduxSaga';
 export { default as withRedux } from './utils/utils/next/withRedux';
