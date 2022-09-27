17b34eeb03dae187f6b4cf1ae6ed872c0da6628f
chrissieaerospace <chrissieaerospace@gmail.com>
2022-09-27 15:39:31 +0530

    [CLEANED-UP] - CODE

--- src/utils/commonReduxSagaConverter/commonGenerator.js
+++ src/utils/commonReduxSagaConverter/commonGenerator.js
@@ -108,6 +108,9 @@ export default function({
     if (!__payload) {
       yield call(loaderGenerator, {
         type,
+        commonData: {
+          status: 'request query not found.Unable to fetch api.',
+        },
       });
       return;
     }
