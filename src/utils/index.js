6954fcaddd4de2f5bb8ff4e52d739b77317504ac
chrissieaerospace <chrissieaerospace@gmail.com>
2023-06-28 04:05:43 +0530

    [MODIFIED]

--- src/utils/index.js
+++ src/utils/index.js
@@ -64,7 +64,7 @@ export const commmonStateHandler = ({
       task = {},
       dontUpdateReducer,
       dontUpdateReducerOnCall,
-      request = {},
+      request,
     } = {},
   } = action;
   if (request && request.payload) delete request.payload;
