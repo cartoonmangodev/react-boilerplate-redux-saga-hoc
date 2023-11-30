ecbf1dc60996ad907d7f357dd15b10a9afd56d6d
chrissieaerospace <chrissieaerospace@gmail.com>
2023-11-30 19:21:48 +0530

    [MODIFIED]

--- src/hooks/Form/form.js
+++ src/hooks/Form/form.js
@@ -1,18 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import isEqual from 'fast-deep-equal';
 import FormContext from './context';
+
 export default ({
   children,
   idKey,
   onSubmit,
   formRef,
   inputProps: _inputProps,
+  extraProps,
 }) => {
+  const ref = useRef(extraProps);
   const [inputProps, setInputProps] = useState(
-    () => _inputProps || formRef.getInputProps(),
+    () => _inputProps || formRef.getInputProps(extraProps),
   );
   useEffect(() => {
     if (formRef && formRef.formId) formRef.setInputProps = setInputProps;
   }, []);
+
+  const is_equal = isEqual(ref.current, extraProps);
+
+  useEffect(() => {
+    if (!is_equal) {
+      ref.current = extraProps;
+      setInputProps(formRef.getInputProps(extraProps));
+    }
+  }, [is_equal]);
+
   const __inputProps = _inputProps || inputProps;
   return (
     <FormContext.Provider value={{ inputProps: __inputProps, idKey, onSubmit }}>
